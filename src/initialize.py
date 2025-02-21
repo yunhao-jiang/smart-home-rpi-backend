85070117831fc9dae3ac6efcf771ebeb63599b65
Robin Han <hrb0755@gmail.com>
2025-02-20 16:43:29 -0800

    feat: Add IR receive functionality

--- src/initialize.py
+++ src/initialize.py
@@ -20,19 +20,30 @@ def initialize_menu():
 
     #  add more function such as receiving IR signals and etc. Do NOT implement this part yet.
 
-    # def ir_send():
-    #     # Send the IR signal
-    #     subprocess.run(["ir-ctl", "-d", "/dev/lirc0", f"--send={filename}"])
-    #     lcd.text("Sent: " + filename,1)
-    #     time.sleep(1)
-    #     # lcd.text(output,1)
+    def ir_send(file):
+        # Send the IR signal
+        subprocess.run(["ir-ctl", "-d", "/dev/lirc0", f"--send={file}"])
+        lcd.text("Sent: " + file, 1)
+        lcd.text("", 2)
+        time.sleep(1)
+        # lcd.text(output,1)
     
-
-    # def ir_receive():
-    #     # Receive the IR signal
-    #     subprocess.run(["ir-ctl", "-d", "/dev/lirc1", f"--receive={filename}", "-1"])
-    #     # Print the IR signal to the LCD
-    #     lcd.text("Received: " + filename,1)
+    file_idx = 0
+    def ir_receive():
+        nonlocal file_idx
+        filename = f"test_{file_idx}.txt"
+        try:
+            lcd.text("Receiving IR...".center(16), 1)
+            lcd.text("Timeout in 10sec".center(16), 2)
+            result = subprocess.run(
+                ["ir-ctl", "-d", "/dev/lirc1", f"--receive={filename}", "-1"],
+                timeout=10
+            )
+            new_option = MenuOptions(name=f"root-ir-list-{filename}", line1="IR List", line1_marker=False, line2=f"{filename[:-4]}", line2_marker=True, action=None, parent=root_ir_list)
+            file_idx += 1
+            return root_ir_add_success
+        except subprocess.TimeoutExpired:
+            return root_ir_add_timeout
     
     
     
@@ -47,10 +58,17 @@ def initialize_menu():
     root_about = MenuOptions(name="root-about", line1="ABOUT", line1_marker=True, line2="", line2_marker=False, action=about_page, parent=root) # the actioin is a callable function definied above that will display the about page, since it's not going to return anything BUT a workflow (has actioni), it will stay at the current node
 
     # Second level - IR
-    root_ir_list= MenuOptions(name="root-ir-list", line1="IR", line1_marker=False, line2="List", line2_marker=True, action=None, parent=root_ir)
-    root_ir_add = MenuOptions(name="root-ir-add", line1="IR", line1_marker=False, line2="Add", line2_marker=True, action=None, parent=root_ir)
+    root_ir_list = MenuOptions(name="root-ir-list", line1="IR", line1_marker=False, line2="List", line2_marker=True, action=None, parent=root_ir)
     root_ir_back = MenuOptions(name="root-ir-back", line1="IR", line1_marker=False, line2="Back", line2_marker=True, action=lambda: root_ir_back.parent, parent=root_ir) # this lambda function allow it to serve as a BACK button (i.e., go to parent node)
+    root_ir_add = MenuOptions(name="root-ir-add", line1="IR", line1_marker=False, line2="Add", line2_marker=True, action=ir_receive, parent=root_ir)
+    # root_ir_add = MenuOptions(name="root-ir-add", line1="IR", line1_marker=False, line2="Add", line2_marker=True, action=ir_receive, parent=root_ir)
 
     # TODO: More menu options can be added here
+    # Third level - IR List
+    root_ir_list_back = MenuOptions(name="root-ir-list-back", line1="IR List", line1_marker=False, line2="Back", line2_marker=True, action=lambda: root_ir_list_back.parent, parent=root_ir_list) 
+
+    # Third level - IR Add
+    root_ir_add_success = MenuOptions(name="root-ir-add-success", line1="IR", line1_marker=False, line2="Add Success", line2_marker=False, action=lambda: root_ir_add_success.parent, parent=root_ir_add)
+    root_ir_add_timeout = MenuOptions(name="root-ir-add-timeout", line1="IR", line1_marker=False, line2="Add Timeout", line2_marker=False, action=lambda: root_ir_add_timeout.parent, parent=root_ir_add)
 
     return Menu(root, lcd)
